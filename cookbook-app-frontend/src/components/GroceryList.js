4922fd50de083f5968d4410608ed68dafd3c46bf
jyk595 <jyk595@gmail.com>
2021-08-24 21:31:20 -0700

    rendered grocery list

--- cookbook-app-frontend/src/components/GroceryList.js
+++ cookbook-app-frontend/src/components/GroceryList.js
@@ -1,6 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import GroceryListItem from './GroceryListItem';
+
 function GroceryList() {
+  const [groceryListData, setGroceryListData] = useState([]);
+
+  useEffect(() => {
+    // fetch(`${process.env.REACT_APP_API_URL}/shopping_items`)
+    fetch(`http://localhost:9292/grocery_list`)
+    .then(res=>res.json())
+    .then(setGroceryListData)
+  },[]);
+
+  const renderChecklist = groceryListData.map(checklistItem => {
+    return <GroceryListItem checklistItem={checklistItem} key={checklistItem.id} />
+  })
+
   return (
-    <h1>This is the grocery list page</h1>
+    <div>
+      <h1>This is the grocery list page</h1>
+      {renderChecklist}  
+    </div>
   )
 }
 
